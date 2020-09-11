07b26197ae8719687313b69a55ce4f740dd716fc
liuxiaokun <liuxiaokun0410@qq.com>
2020-09-11 15:39:52 +0800

    二级缓存开启

--- src/main/java/com/example/quickdev/entity/UserTest.java
+++ src/main/java/com/example/quickdev/entity/UserTest.java
@@ -2,13 +2,15 @@ package com.example.quickdev.entity;
 
 import lombok.Data;
 
+import javax.naming.directory.SearchResult;
+import java.io.Serializable;
 import java.util.Date;
 
 /**
  * @author liuxiaokun
  */
 @Data
-public class UserTest {
+public class UserTest implements Serializable {
 
     private Long id;
     private String username;
